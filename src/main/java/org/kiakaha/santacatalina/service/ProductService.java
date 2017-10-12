cec231fc7c95a8b2f9dc04e1d36d657e12d86570
ignaciovarela <ignaciovarela@192.168.0.62>
2017-10-11 21:34:57 -0300

    Subo el productService y productResources (servicio que interactua con la base y los servicios rest)

---
+++ src/main/java/org/kiakaha/santacatalina/service/ProductService.java
@@ -0,0 +1,52 @@
+package org.kiakaha.santacatalina.service;
+
+import java.util.List;
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+
+import org.kiakaha.santacatalina.ProductRepository;
+import org.kiakaha.santacatalina.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	
+	private ProductRepository productRepository;
+	 
+	@Autowired
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+ 
+	public Product save(Product product) {
+		if (product.getId() != null && productRepository.exists(product.getId())) {
+			throw new EntityExistsException("There is already existing entity with such ID in the database.");
+		}
+ 
+		return productRepository.save(product);
+	}
+ 
+	public Product update(Product product) {
+		if (product.getId() != null && !productRepository.exists(product.getId())) {
+			throw new EntityNotFoundException("There is no entity with such ID in the database.");
+		}
+ 
+		return productRepository.save(product);
+	}
+ 
+	public List<Product> findAll() {
+		return (List<Product>) productRepository.findAll();
+	}
+ 
+	public Product findOne(Long id) {
+		return productRepository.findOne(id);
+	}
+ 
+	public void delete(Long id) {
+		productRepository.delete(id);
+	}
+		
+
+}
