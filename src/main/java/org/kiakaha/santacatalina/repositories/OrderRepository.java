0a095f6747ea51c06fdeec35a3af7e7add7f625e
sebastian <sebastiansanio@outlook.com>
2017-10-22 11:42:52 -0300

    Corrección en OrderRepository.

--- src/main/java/org/kiakaha/santacatalina/repositories/OrderRepository.java
+++ src/main/java/org/kiakaha/santacatalina/repositories/OrderRepository.java
@@ -1,11 +1,11 @@
 package org.kiakaha.santacatalina.repositories;
 
-import org.kiakaha.santacatalina.model.Category;
+import org.kiakaha.santacatalina.model.Order;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(excerptProjection = InLineCategory.class,collectionResourceRel = "orders", itemResourceRel = "order")
-public interface OrderRepository extends PagingAndSortingRepository<Category, Long>  {
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>  {
 	
 
 }
